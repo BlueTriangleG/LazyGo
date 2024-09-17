6d92aee21181042515b68891618d6a1cdda47a07
GAOYUAN HAO <123238461+BlueTriangleG@users.noreply.github.com>
2024-09-17 12:17:30 +1000

    fix pakage bugs

--- constants/index.ts
+++ constants/index.ts
@@ -24,20 +24,16 @@ import star from '@/assets/icons/star.png'
 import target from '@/assets/icons/target.png'
 import to from '@/assets/icons/to.png'
 import check from '@/assets/images/check.png'
-import getStarted from '@/assets/images/get-started.png'
 import message from '@/assets/images/message.png'
 import noResult from '@/assets/images/no-result.png'
 import onboarding1 from '@/assets/images/onboarding1.png'
 import onboarding2 from '@/assets/images/onboarding2.png'
 import onboarding3 from '@/assets/images/onboarding3.png'
-import signUpCar from '@/assets/images/signup-car.png'
 
 export const images = {
   onboarding1,
   onboarding2,
   onboarding3,
-  getStarted,
-  signUpCar,
   check,
   noResult,
   message,
