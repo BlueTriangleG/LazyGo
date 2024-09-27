e470fc8c2b968e97a549fb247028c3cd8f410699
GAOYUAN HAO <123238461+BlueTriangleG@users.noreply.github.com>
2024-09-27 15:45:46 +1000

    modify button

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -7,9 +7,20 @@ export default function Page() {
   return (
     <View>
       <Text style={commonStyles.h1text}>Welcome Lazy Go</Text>
-      <CustomButton title="Go to Map" onPress={async () => {
-        router.replace('/(root)/(tabs)/gpt_test')
-      }} />
+      <CustomButton
+        className="mt-6"
+        title="Go to Map"
+        onPress={async () => {
+          router.replace('/(root)/(tabs)/map-test')
+        }}
+      />
+      <CustomButton
+        className="mt-6"
+        title="generate plan"
+        onPress={async () => {
+          router.replace('/(root)/(tabs)/gpt_test')
+        }}
+      />
     </View>
   )
 }
