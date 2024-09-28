42c4e5fb0cab710cdf84f8776d5556cbcfb13b05
Blueboylee <74399143+Blueboylee@users.noreply.github.com>
2024-09-28 22:18:39 +1000

    update map marker

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import MapView, { Marker, Polyline } from 'react-native-maps';
 
 // 定义 latDataCoords 类型
@@ -34,7 +34,16 @@ const Map: React.FC<MapProps> = ({ coords }) => {
             coordinate={{ latitude: location.lat, longitude: location.long }}
             title={location.title}
             description={location.description}
-          />
+            anchor={{ x: 0.5, y: 1 }} // 将锚点设置为底部中央
+          >
+            <View style={styles.markerContainer}>
+              <Image
+                source={require('@/assets/images/pin.png')} // 自定义标记图像
+                style={styles.markerImage}
+              />
+              <Text style={styles.markerText}>{index + 1}</Text>
+            </View>
+          </Marker>
         ))}
 
         {/* 根据传递的坐标连线 */}
@@ -44,7 +53,7 @@ const Map: React.FC<MapProps> = ({ coords }) => {
               latitude: location.lat,
               longitude: location.long,
             }))}
-            strokeColor="#588157" // 线的颜色
+            strokeColor="#76c893" // 线的颜色
             strokeWidth={3} // 线的宽度
           />
         )}
@@ -60,6 +69,23 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  markerContainer: {
+    position: 'relative', // 相对定位，以便将数字放在图片上面
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  markerImage: {
+    width: 20, // 根据实际图像大小调整
+    height: 20, // 根据实际图像大小调整
+  },
+  markerText: {
+    position: 'absolute', // 绝对定位以将文本放在图像中央
+    fontWeight: 'bold',
+    fontSize: 10, // 字体大小
+    color: '#FFFFFF', // 字体颜色
+    textAlign: 'center',
+    bottom: 5, // 向下偏移一些以确保文本在图标的中心
   },
 });
 
