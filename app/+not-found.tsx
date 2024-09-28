ce1abb1ccd0e49a5950b13375a7784a633bf9d93
GAOYUAN HAO <123238461+BlueTriangleG@users.noreply.github.com>
2024-09-28 16:35:07 +1000

    fix root, tab error

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
-        <Text type="title">This screen doesn't exist.</Text>
+        <Text>This screen doesn't exist.</Text>
         <Link href="/" style={styles.link}>
           <Text type="link">Go to home screen!</Text>
         </Link>
